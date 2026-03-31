a201d64eb42ff683473fdf8d5ebaccf7bab00962
kanishikaverma506 <kanishikaverma8@gmail.com>
2026-04-01 02:40:06 +0530

    comment

---
+++ javascript/comment.js
@@ -0,0 +1,15 @@
+//this is for one line comment
+
+alert('practice');
+alert('comment')
+alert('hmmmm');
+
+alert(3+
+4*2);
+
+/* 
+this is a multi line comment
+*/
+
+alert('nested loop is not supported in javascript.')
+
