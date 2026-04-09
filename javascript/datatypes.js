a6d5ccc7fc3ca70903f27dc15212713eca0bddf1
kanishikaverma506 <kanishikaverma8@gmail.com>
2026-04-09 22:51:28 +0530

    datatypes

---
+++ javascript/datatypes.js
@@ -0,0 +1,17 @@
+let name = "Ilya";
+
+alert( `hello ${1}` ); // hello 1
+
+alert( `hello ${"name"}` ); // hello name
+
+alert( `hello ${name}` ); //  hello Ilya
+
+//8 data types
+alert(typeof 3);
+alert(typeof 4n);
+alert(typeof 'kanishika');
+alert(typeof undefined);
+alert(typeof null);
+alert(typeof Symbol("id"));
+alert(typeof Math);
+alert(typeof true);
