72677e249f60bca46a02211bb5c23be6466a4dd7
kanishikaverma506 <kanishikaverma8@gmail.com>
2026-04-09 22:25:55 +0530

    variables

--- javascript/variable.js
+++ javascript/variable.js
@@ -7,3 +7,10 @@ let  name = 'John' , admin ;
 admin= name;
 alert(admin);
 
+/* TASK 2 : Create a variable with the name of our planet. How would you name such a variable?
+Create a variable to store the name of a current visitor to a website. 
+How would you name that variable? */
+
+let planet = 'Earth';
+let currentVisitor ;
+
